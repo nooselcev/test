b47455a4b4ff73b59f8b0984968cb6fb4fbca043
nooselcev <root@localhost.localdomain>
2021-03-03 12:58:48 +0300

    change test.py file

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 
+import time
+
+
 def test():
     print('OK')
     print('Hello World')
 
 
 if __name__ == '__main__':
-    test()
+    while True:
+      test()
+      time.sleep(5)
 
